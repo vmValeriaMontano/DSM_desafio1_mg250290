57b91f644646e668420bc7db24f710354bc790a7
valeria.montano <montanovaleria55@gmail.com>
2026-08-12 12:05:18 -0600

    actualizar: layouts promedio, descuento y calculadora muestran un mensaje de prueba de funcionalidad, main muestra 3 botones en estos archivos se usa LineraLayout y Button. En MainActivity.kt se usa logica para la prueba de los botones haciendo uso de Intent y setOnClickListener

--- app/src/main/java/com/example/desafio1_dsm/MainActivity.kt
+++ app/src/main/java/com/example/desafio1_dsm/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.desafio1_dsm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+        btn1 = findViewById(R.id.button1)
+        btn2 = findViewById(R.id.button2)
+        btn3 = findViewById(R.id.button3)
+
+        btn1.setOnClickListener {
+            // Acción para el Botón 1
+            val ir= Intent(this, Promedio::class.java)
+            startActivity(ir)
+        }
+
+        btn2.setOnClickListener {
+            // Acción para el Botón 2
+            val ir= Intent(this, Descuentos::class.java)
+            startActivity(ir)
+
+        }
+
+        btn3.setOnClickListener {
+            // Acción para el Botón 3
+            val ir= Intent(this, Calculadora::class.java)
+            startActivity(ir)
+
+        }
+
+    }//Fin onCreate
 }
